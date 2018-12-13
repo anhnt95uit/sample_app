c7459a5d8b824eb36c6258064764882691439566
anhnt <anhnt95.uit@gmail.com>
2018-12-13 10:45:36 +0700

    Finish chapter_11

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,24 +3,11 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by email: params[:session][:email].downcase
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      if params[:session][:remember_me] == Settings.session.one
-        remember(user)
-      else
-        forget(user)
-      end
-      remember user
-      redirect_back_or user
-    else
-      flash[:danger] = t "dictionary.danger" # Not quite right!
-      render :new
-    end
+    check_login user, params
   end
 
   def destroy
     log_out if logged_in?
-    log_out
     redirect_to root_url
   end
- end
+end
